52fb5c9bc8f6683003357f10dd28f5eeae1069bf
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2022-04-22 20:25:18 +0700

    update: Load Konten Error

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -1,31 +1,38 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Barang extends CI_Controller {
+class Barang extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Barang_model');
+	}
 
 	public function list_barang()
 	{
 		$data_barang = $this->Barang_model->get_barang();
 
-        $konten = '<tr>
+		$konten = '<tr>
             <td>Nama</td>
             <td>Deskripsi</td>
             <td>Stock</td>
             <td>Aksi</td>
         </tr>';
 
-        foreach ($data_barang->result() as $key => $value) {
-            $konten = '<tr>
-                            <td>'.$value->nama_barng.'</td>
-                            <td>'.$value->deskripsi.'</td>
-                            <td>'.$value->stok.'</td>
+		foreach ($data_barang->result() as $key => $value) {
+			$konten .= '<tr>
+                            <td>' . $value->nama_barang . '</td>
+                            <td>' . $value->deskripsi . '</td>
+                            <td>' . $value->stok . '</td>
                             <td>Read | Hapus | Edit</td>
-                    </tr>';
-        }
-        $data_json = array(
-            'konten' => $konten,
-        );
+                    	</tr>';
+		}
 
-        echo json_encode($data_json);
+		$data_json = array(
+			'konten' => $konten
+		);
+
+		echo json_encode($data_json);
 	}
 }
