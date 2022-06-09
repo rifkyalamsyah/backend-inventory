32fcb156532b479e99bcf5a6510cdd004cfe0ed5
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2022-06-10 02:13:29 +0700

    Fitur CRUD: Input dan Update Data

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -9,6 +9,7 @@ class Barang extends CI_Controller
 		$this->load->model('Barang_model');
 	}
 
+
 	public function list_barang()
 	{
 		$data_barang = $this->Barang_model->get_barang();
@@ -16,7 +17,7 @@ class Barang extends CI_Controller
 		$konten = '<tr>
             <td>Nama</td>
             <td>Deskripsi</td>
-            <td>Stock</td>
+            <td>Stok</td>
             <td>Aksi</td>
         </tr>';
 
@@ -25,14 +26,77 @@ class Barang extends CI_Controller
                             <td>' . $value->nama_barang . '</td>
                             <td>' . $value->deskripsi . '</td>
                             <td>' . $value->stok . '</td>
-                            <td>Read | Hapus | Edit</td>
-                    	</tr>';
+                            <td>Read | Hapus | <a href="#' . $value->id_barang . '" class="linkEditBarang">Edit</a></td>
+                    </tr>';
 		}
 
 		$data_json = array(
-			'konten' => $konten
+			'konten' => $konten,
 		);
 
 		echo json_encode($data_json);
+	}
+
+
+	public function create_action()
+	{
+		$this->db->trans_start();
+
+		$arr_input = array(
+			'nama_barang' => $this->input->post('nama_barang'),
+			'deskripsi' => $this->input->post('deskripsi'),
+		);
+
+		$this->Barang_model->insert_data($arr_input);
+
+		if ($this->db->trans_status() === FALSE) {
+			$this->db->trans_rollback();
+			$data_output = array('sukses' => 'tidak', 'pesan' => 'Gagal Input Data Barang');
+		} else {
+			$this->db->trans_commit();
+			$data_output = array('sukses' => 'ya', 'pesan' => 'Berhasil Input Data Barang');
+		}
+
+		echo json_encode($data_output);
+	}
+
+
+	public function detail()
+	{
+		$id_barang = $this->input->get('id_barang');
+		$data_detail = $this->Barang_model->get_by_id($id_barang);
+
+		if ($data_detail->num_rows() > 0) {
+			$data_output = array('sukses' => 'ya', 'detail' => $data_detail->row_array());
+		} else {
+			$data_output = array('sukses' => 'tidak');
+		}
+
+		echo json_encode($data_output);
+	}
+
+
+	public function update_action()
+	{
+		$this->db->trans_start();
+
+		$id_barang = $this->input->post('id_barang');
+
+		$arr_input = array(
+			'nama_barang' => $this->input->post('nama_barang'),
+			'deskripsi' => $this->input->post('deskripsi'),
+		);
+
+		$this->Barang_model->update_data($id_barang, $arr_input);
+
+		if ($this->db->trans_status() === FALSE) {
+			$this->db->trans_rollback();
+			$data_output = array('sukses' => 'tidak', 'pesan' => 'Gagal Update Data Barang');
+		} else {
+			$this->db->trans_commit();
+			$data_output = array('sukses' => 'ya', 'pesan' => 'Berhasil Update Data Barang');
+		}
+
+		echo json_encode($data_output);
 	}
 }
