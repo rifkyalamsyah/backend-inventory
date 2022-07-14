57db75c943653441291bab3e09491ca2194b2366
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2022-07-14 22:18:04 +0700

    Fitur Upload

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -15,23 +15,25 @@ class Barang extends CI_Controller
 		$data_barang = $this->Barang_model->get_barang();
 
 		$konten = '<tr>
-            <td>Nama</td>
-            <td>Deskripsi</td>
-            <td>Stok</td>
-            <td>Aksi</td>
-        </tr>';
+						<td>Nama</td>
+						<td>Deskripsi</td>
+						<td>Stok</td>
+						<td>Foto</td>
+						<td>Aksi</td>
+					</tr>';
 
 		foreach ($data_barang->result() as $key => $value) {
 			$konten .= '<tr>
-                            <td>' . $value->nama_barang . '</td>
-                            <td>' . $value->deskripsi . '</td>
-                            <td>' . $value->stok . '</td>
-                            <td>Read 
-							| <a href="#' . $value->id_barang . '" class="linkHapusBarang">Hapus</a>
-							| <a href="#' . $value->id_barang . '" class="linkEditBarang">Edit</a></td>
-                    </tr>';
+							<td>' . $value->nama_barang . '</td>
+							<td>' . $value->deskripsi . '</td>
+							<td>' . $value->stok . '</td>
+							<td><img src="' . base_url() . '/foto/' . $value->id_barang . '/' . $value->foto_produk . '"
+							width="50"></td>
+							<td>Read | <a href="#' . $value->id_barang . '" class="linkHapusBarang"
+							>Hapus</a> | <a href="#' . $value->id_barang . '" class="linkEditBarang" >Edit</a></td>
+							</tr>';
 		}
-
+		
 		$data_json = array(
 			'konten' => $konten,
 		);
@@ -47,10 +49,15 @@ class Barang extends CI_Controller
 		$arr_input = array(
 			'nama_barang' => $this->input->post('nama_barang'),
 			'deskripsi' => $this->input->post('deskripsi'),
-			'stok' => $this->input->post('stok'),
 		);
 
 		$this->Barang_model->insert_data($arr_input);
+
+		$id_barang = $this->db->insert_id();
+
+		if ($_FILES != null) {
+			$this->upload_foto($id_barang, $_FILES);
+		}
 
 		if ($this->db->trans_status() === FALSE) {
 			$this->db->trans_rollback();
@@ -88,10 +95,13 @@ class Barang extends CI_Controller
 		$arr_input = array(
 			'nama_barang' => $this->input->post('nama_barang'),
 			'deskripsi' => $this->input->post('deskripsi'),
-			'stok' => $this->input->post('stok'),
 		);
 
 		$this->Barang_model->update_data($id_barang, $arr_input);
+
+		if ($_FILES != null) {
+			$this->upload_foto($id_barang, $_FILES);
+		}
 
 		if ($this->db->trans_status() === FALSE) {
 			$this->db->trans_rollback();
@@ -176,5 +186,41 @@ class Barang extends CI_Controller
 		);
 
 		echo json_encode($data_json);
+	}
+
+
+	private function upload_foto($id_barang, $files)
+	{
+		$gallerPath = realpath(APPPATH . '../foto');
+		$path = $gallerPath . '/' . $id_barang;
+		if (!is_dir($path)) {
+			mkdir($path, 0777, TRUE);
+		}
+
+		$konfigurasi = array(
+			'allowed_types' => 'jpg|png|jpeg',
+			'upload_path' => $path,
+			'overwrite' => true
+		);
+
+		$this->load->library('upload', $konfigurasi);
+
+		$_FILES['file']['name'] = $files['file']['name'];
+		$_FILES['file']['type'] = $files['file']['type'];
+		$_FILES['file']['tmp_name'] = $files['file']['tmp_name'];
+		$_FILES['file']['error'] = $files['file']['error'];
+		$_FILES['file']['size'] = $files['file']['size'];
+
+		if ($this->upload->do_upload('file')) {
+			$data_barang = array(
+				'foto_produk' => $this->upload->data('file_name')
+			);
+
+			$this->Barang_model->update_data($id_barang, $data_barang);
+
+			return 'Success Upload';
+		} else {
+			return 'Error Upload';
+		}
 	}
 }
